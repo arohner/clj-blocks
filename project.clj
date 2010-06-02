8d9be898b03b9bbbacb1add1a6ca36613b00ca18
Allen Rohner <arohner@gmail.com>
2010-06-02 15:42:24 -0500

    Update hiccup snapshot to one that contains the fix for option tags being self closing

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [sandbar "0.2.3"]
-                 [hiccup "0.2.3"]
+                 [org.clojars.arohner/hiccup "89b83e15fc06b05352d55cbf9bea8a9833875f42"]
                  [clout "0.2.0"]
                  [compojure "0.4.0-SNAPSHOT"]
 		 [ring/ring-core "0.2.0"]
