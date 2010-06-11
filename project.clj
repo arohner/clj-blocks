2f8f8bc17155d6c5cb2108c93ef3668271433551
Allen Rohner <arohner@gmail.com>
2010-06-11 09:57:09 -0500

    Bump the version number

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-blocks
-  "0.0.1"
+  "0.0.2"
   :description "a web framework inspired by Weblocks"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
@@ -10,7 +10,7 @@
 		 [ring/ring-core "0.2.0"]
 		 [ring/ring-devel "0.2.0"]
 		 [ring/ring-jetty-adapter "0.2.0"]
-                 [scriptjure "0.1.7"]]
+                 [scriptjure "0.1.9"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]
 		     [swank-clojure "1.2.0-SNAPSHOT"]
 		     [jline "0.9.94"]])
