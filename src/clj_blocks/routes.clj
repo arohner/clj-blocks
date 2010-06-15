2a4c877755e3891f61707415454df14228e04b58
Allen Rohner <arohner@gmail.com>
2010-06-14 21:48:56 -0500

    Adds wrap-read-view, def-webfn

--- src/clj_blocks/routes.clj
+++ src/clj_blocks/routes.clj
@@ -1,4 +1,5 @@
 (ns clj-blocks.routes
+  (:use [clj-blocks.core :only (read-view)])
   (:use clj-blocks.utils)
   (:use [clojure.contrib.except :only (throwf)])
   (:require ring.middleware.keyword-params)
@@ -14,33 +15,36 @@
   (let [defn-args (cons name defn-args)
         arg-map (apply decompose-defn-args* defn-args)
         arg-map (update-in arg-map [:attr-map] merge {::http-method http-method
-                                                      ::http-path http-path})]
+                                                      ::http-path http-path
+                                                      ::bindings `(quote ~(:params arg-map))})]
     `(defn ~@(defn-map* arg-map))))
 
 (defn find-route-fns
   "introspects the namespace, returns a seq of [fn-name http-method path] vecs"
   [ns]
-  (for [[name var-fn] (ns-publics ns) :when (::http-method (meta var-fn))]
-    [(keyword name) (::http-method (meta var-fn)) (::http-path (meta var-fn))]))
+  (for [[name var-fn] (ns-publics ns) :when (::http-method (meta var-fn))
+        :let [metadata (meta var-fn)]]
+    [(keyword name) (::bindings metadata) (::http-method metadata) (::http-path metadata)]))
 
+(defn ns-routes* [ns-symbol]
+  (require ns-symbol)
+  (let [ns (find-ns ns-symbol)]
+    (->>
+     (for [[fn-name fn-binding http-method path] (find-route-fns ns)]
+       (do
+         (assert fn-name)
+         (assert ns)
+         (let [f (ns-resolve ns (symbol (clojure.core/name fn-name)))
+               path (format path name)]
+           (when f
+             (println "adding route" http-method path "->" fn-name)
+             [http-method path fn-binding f]))))
+     (filter identity))))
+  
 (defmacro ns-routes
   "Returns the set of routes in ns defined using defroutefn"
-  [ns-symbol]
-  (require ns-symbol)
-  (let [ns (find-ns ns-symbol)
-        routes (->>
-                (for [[fn-name http-method path] (find-route-fns ns)]
-                  (do
-                    (assert fn-name)
-                    (assert ns)
-                    (let [f (ns-resolve ns (symbol (clojure.core/name fn-name)))
-                          path (format path name)]
-                      (when f
-                        (println "adding route" http-method path "->" fn-name)
-                        (@(var compojure/compile-route) http-method path {:as 'request} [`(~f (update-in ~'request [:params] (var ring.middleware.keyword-params/keyify-params)))])))))
-                (filter identity)
-                (into []))]
-    `(apply compojure/routes ~routes)))
+  [ns]
+  `(ns-routes* (quote ~ns)))
 
 (defn path-for* [routefn route-args]
   {:pre [routefn]}
@@ -55,6 +59,52 @@
   "Returns the URL path for a var defined using defroutefn. If the route contains any variables, i.e. /foo/:id, route-args is a map of keywords to values to replace.
 
   path-for resolves vars at runtime (not compile time)"
+  
   [routefn & [route-args]]
   (let [curr-ns *ns*]
     `(path-for* (ns-resolve ~curr-ns (quote ~routefn)) ~route-args)))
+
+(defn apply-map
+  "Like apply, but if the last argument is a map, will convert the map to work with fn signatures that ends with a map destructuring. i.e.
+
+   (defn foo [ a b & {opt1 :opt1 opt2 :opt2}])
+   (apply foo 1 2 {:opt1 42 :opt2 3.14})"
+  [f & args]
+  (let [args (if (map? (last args))
+                 (concat (butlast args) (interleave (keys (last args)) (vals (last args))))
+                 args)]
+    (apply f args)))
+
+(defn wrap-read-view [handler]
+  "middleware. If the request has a :request-fn key and the request-fn
+has a :view metadata, will read-view the params. You probably want to
+insert this between compojure's find-matching-route and
+call-matching-route."
+  (fn [request]
+    (if (and (:route-fn request) (::view (meta (:route-fn request))))
+      (let [view (::view (meta (:route-fn request)))
+            request (update-in request [:params] #(read-view view %))]
+        (handler request))
+      (handler request))))
+
+(defmacro def-webfn
+  "Creates a function that can be called normally, or served as an
+  HTTP request. View is a view created with defview.
+
+  When the fn is called as an HTTP request, the arguments will be
+  taken from the http params map, read using read-view, and then
+  passed to the fn. If the function has & args, the & arg will be a
+  map containing params in the request map that weren't bound to a
+  positional argument.
+
+  When the fn is called in normal clojure code, read-view will not be
+  called, so the the argument should be the correct type
+
+  Requires the wrap-read-view middleware"
+  
+  [name [http-method http-path] view signature & body]
+  `(defroutefn ~name [~http-method ~http-path] {::view ~view} ~signature
+     ~@body))
+
+(defn def-block )
+
