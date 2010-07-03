ddaddb847914ab7ceda59d589cb8609044bce8e8
Allen Rohner <arohner@gmail.com>
2010-07-03 16:21:05 -0500

    Update project dependencies, bump version number

--- project.clj
+++ project.clj
@@ -1,15 +1,17 @@
 (defproject clj-blocks
-  "0.0.5"
+  "0.0.6"
   :description "a web framework inspired by Weblocks"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [sandbar "0.2.3"]
-                 [org.clojars.arohner/hiccup "44cbc6e1a1534d1ad32b8bbdf6424d3ecd788192"]
+                 [sandbar "0.2.3" :exclusions [hiccup ring/ring-core sandbar-session compojure]]
+                 [sandbar/sandbar-core "0.3.0" :exclusions [hiccup]]
+                 [org.clojars.arohner/sandbar-session "cd7bb1a8ad844dbef623a76353a6529ed3ce1f67"]
+                 [org.clojars.arohner/hiccup "9bab3bd8bedbfcbd13e2ba278798615dd2895346"]
                  [clout "0.2.0"]
                  [org.clojars.arohner/compojure "41a61347414ac54eadc496fb5bb7f4b25955cd4c"]
-		 [ring/ring-core "0.2.0"]
-		 [ring/ring-devel "0.2.0"]
-		 [ring/ring-jetty-adapter "0.2.0"]
+		 [org.clojars.arohner/ring-core "f47973382a035d67bd8f9d2890de40aabd65b45f" :exclusions [hiccup compojure]]
+		 [ring/ring-devel "0.2.3" :exclusions [ring/ring-core hiccup compojure]]
+		 [ring/ring-jetty-adapter "0.2.3" :exclusions [ring/ring-core]]
                  [scriptjure "0.1.9"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]
 		     [swank-clojure "1.2.0-SNAPSHOT"]
