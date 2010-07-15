8e2742d8033cdb7ca62c548d2230f8eaaf1e70e4
Allen Rohner <arohner@gmail.com>
2010-07-15 14:30:28 -0500

    Adds a render-view dispatch for data

---
+++ src/clj_blocks/data.clj
@@ -0,0 +1,10 @@
+(in-ns 'clj-blocks.core)
+
+(defmethod render-view-blocks :data [blocks _]
+  [:div {:class "clj-block-data"}
+   (for [block blocks
+         :let [body (render-block block :data)]]
+     [:div {:class "clj-block-value"}
+      (with-label block
+        body)
+      body])])
