e5f43940b518a8e1bb7a563230778104fae55fab
Allen Rohner <arohner@gmail.com>
2011-04-09 16:39:14 -0500

    Remove the sandbar dependency, add contrib to reflect the fact that we depend on contrib.

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   "0.0.23"
   :description "a web framework inspired by Weblocks"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 ;; [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.arohner/sandbar "4c48185af93b5a86f5c5c38b8e2bcc8a8a676bc0"] ;; Snapshot of 0.4.0. custom build because we need sandbar commit 4c48185af93b5a86f5c5c38b8e2bcc8a8a676bc0. Can move to official sandbar in the first 0.4 release
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [clout "0.4.1"]                 
                  [ring/ring "0.3.7"]
                  [hiccup/hiccup "0.3.4"]
